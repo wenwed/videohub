95df696a278d7b680923bbc225bb186466fedc82
wenwed <1583925120@qq.com>
2020-08-28 02:46:32 +0800

    update

---
+++ client/src/router.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import homeView from '@/views/homeView.vue'
+
+Vue.use(Router)
+
+const vueRouter = new Router({
+  routes: [
+    { path: '/', redirect: '/home' },
+    { path: 'home', name: "首页", component: homeView }
+  ]
+})
+
+export default vueRouter
