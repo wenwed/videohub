455962770f9566c11512ded835f7fdd33d4f971b
wenwed <1583925120@qq.com>
2020-09-17 23:53:59 +0800

    update

--- client/src/components/videoPlayer.vue
+++ client/src/components/videoPlayer.vue
@@ -9,11 +9,19 @@
       @click="changePlay">
     </video>
     <div class="controller">
-      <input type="button" :value="btnvalue" @click="changePlay">
-      <span id="curtime">{{ curtime }}</span>/
-      <span id="duration">{{ durationtime }}</span>
-      <span><i class="el-icon-microphone"></i></span>
-      <span><i class="el-icon-full-screen" @click="fullScreen"></i></span>
+      <div class="controller-bar">
+        <div class="load-bar">
+          <div class="play-bar"></div>
+        </div>
+      </div>
+      <br />
+      <div class="controller-footer">
+        <input type="button" :value="btnvalue" @click="changePlay">
+        <span id="curtime">{{ curtime }}</span>/
+        <span id="duration">{{ durationtime }}</span>
+        <span><i class="el-icon-microphone"></i></span>
+        <span><i class="el-icon-full-screen" @click="fullScreen"></i></span>
+      </div>
     </div>
   </div>
 </template>
@@ -23,9 +31,10 @@ export default {
   props: [ "videoInfo" ],
   data(){
     return {
-      curtime: 0,
-      durationtime: 0,
+      curtime: "00:00",
+      durationtime: "00:00",
       btnvalue: "play",
+      timer: null
     }
   },
   methods: {
@@ -33,12 +42,48 @@ export default {
       let player = this.$refs.videoplayer;
       if(player.paused){
         player.play();
-        this.durationtime = player.duration;
+        this.durationtime = this.formatTime(player.duration);
+        this.startMonitor();
         this.btnvalue = "pause";
       }else{
         player.pause();
+        this.stopMonitor();
         this.btnvalue = "play";
       }
+    },
+    startMonitor() {
+      this.timer = setInterval(() => {
+        let player = this.$refs.videoplayer;
+        this.curtime = this.formatTime(player.currentTime);
+      },500)
+    },
+    stopMonitor() {
+      clearInterval(this.timer);
+      this.timer = null;
+    },
+    formatTime(time) {
+      let res ="";
+      //小时
+      let h = Math.floor(time/3600);
+      console.log(h);
+      //分钟
+      let m = Math.floor(time/60);
+      //秒
+      let s = Math.floor(time%60);
+      if(h !== 0){
+        res = h + ":";
+      }
+      if(m.toString().length === 1){
+        res = res + "0" + m + ":";
+      }else{
+        res = res + m + ":";
+      }
+      if(s.toString().length === 1){
+        res = res + "0" + s;
+      }else{
+        res = res + s;
+      }
+      return res;
     },
     fullScreen() {
       let player = this.$refs.videoplayer;
@@ -70,9 +115,29 @@ export default {
   }
   .controller{
     width: 100%;
-    height: 20px;
+    height: 50px;
     color: white;
     background-color: black;
+    .controller-bar{
+      height: 20px;
+      width: 100%;
+      background-color: rgb(236, 233, 233);
+      .load-bar{
+        width: 80%;
+        text-align: right;
+        line-height: 20px;
+        background-color: rgb(189, 188, 188);
+        .play-bar{
+          width: 50%;
+          text-align: right;
+          line-height: 5px;
+          background-color: rgb(238, 161, 208);
+        }
+      }
+    }
+    .controller-footer{
+      display: flex;
+    }
   }
 }
 </style>
